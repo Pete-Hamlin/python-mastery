42f6ac82035029b6bf2df9c0bfd4103fd9c7ec7a
pete <work@pete-hamlin.com>
2025-12-23 14:32:13 +0000

    feat: Ex 4.1

---
+++ ex4_1.py
@@ -0,0 +1,15 @@
+class SimpleStock:
+    def __init__(self, name, shares, price):
+        self.name = name
+        self.shares = shares
+        self.price = price
+
+    def cost(self):
+        return self.shares * self.price
+
+
+if __name__ == "__main__":
+    goog = SimpleStock("GOOG", 100, 490.10)
+    ibm = SimpleStock("IBM", 50, 91.23)
+    print(goog.__dict__)
+    print(ibm.__dict__)
